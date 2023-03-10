94725087dc27181fceede099e0ad12e8da61c44f
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-03-10 00:11:13 -0700

    setup + first path

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 8080;
+const mongoUri = process.env.MONGODB_URI;
+const client = new MongoClient(mongoUri);
 
-app.listen(PORT, (error) => {
-  if (!error)
-    console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
-    );
-  else console.log("Error occurred, server can't start", error);
-});
+const dbName = "a2";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+async function start() {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log("Connected successfully to db");
+    const db = client.db(dbName);
+
+    // Set up routes
+    const routes = require("./src/routes")(db);
+    app.use("/api", routes);
+
+    // Start server
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+start();
