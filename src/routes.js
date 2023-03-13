5d871b543c4b3e7d7d7cbc5b7ce74e597e6ad384
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-03-12 23:44:47 -0600

    added limit route

--- src/routes.js
+++ src/routes.js
@@ -24,5 +24,38 @@ module.exports = (db) => {
     }
   });
 
+  // Get number of movies
+  router.get("/movies/limit/:num", async (req, res) => {
+    const { num } = req.params;
+
+    if (isNaN(num) || parseInt(num) < 1) {
+      res.status(400).json({
+        status: "Error",
+        message: "Invalid number of movies requested",
+      });
+      return;
+    }
+
+    try {
+      const cursor = db.collection("movies").find().limit(parseInt(num));
+      const results = await cursor.toArray();
+      const filteredResults = results.map(({ _id, ...rest }) => rest);
+
+      if (filteredResults.length === 0) {
+        res.status(404).json({
+          status: "Error",
+          message: "No movies found",
+        });
+      }
+
+      res.status(200).json(filteredResults.slice(0, num));
+    } catch (err) {
+      res.status(500).json({
+        status: "Error",
+        message: "Internal server error",
+      });
+    }
+  });
+
   return router;
 };
