9278430c3edf400865694146b72a96d1328fb434
bbode837 <bbode837@mtroyal.ca>
2023-03-13 13:18:04 -0600

    index.ejs

--- app.js
+++ app.js
@@ -10,26 +10,32 @@ const client = new MongoClient(mongoUri);
 
 const dbName = "a2";
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const start = async () => {
-  try {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log("Connected successfully to db");
-    const db = client.db(dbName);
+	try {
+		// Use connect method to connect to the server
+		await client.connect();
+		console.log("Connected successfully to db");
+		const db = client.db(dbName);
 
-    // Set up routes
-    const routes = require("./src/routes")(db);
-    app.use("/api", routes);
+		// Set up routes
+		const routes = require("./src/routes")(db);
+		app.use("/api", routes);
 
-    // Start server
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
+		app.get("/", (req, res) => {
+			res.render("pages/index");
+		});
+
+		// Start server
+		app.listen(port, () => {
+			console.log(`Server running on port ${port}`);
+		});
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 start();
